1da5a50e283b52a8bf1b682843efd5627f4edce1
tristan-rvk <tristan.schmale@reykjavik.is>
2023-11-17 18:46:51 +0000

    updated again route.ts to fix type issue

--- app/src/app/api/hello/route.ts
+++ app/src/app/api/hello/route.ts
@@ -1,12 +1,3 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-type ResponseData = {
-  message: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>,
-) {
-  res.status(200).json({ message: 'ok' })
+export default function handler(req, res) {
+  res.status(200).json({ status: 'ok' })
 }
